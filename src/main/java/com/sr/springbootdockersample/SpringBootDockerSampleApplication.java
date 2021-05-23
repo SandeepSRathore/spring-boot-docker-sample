06c39fcd4d3a6474ba018596f10c068d7c731633
SandeepSRathore <sandeeprathore9211@gmail.com>
2021-05-23 16:27:25 +0530

    Sample spring boot app with docker

---
+++ src/main/java/com/sr/springbootdockersample/SpringBootDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sr.springbootdockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sr/springbootdockersample/SpringBootDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.springbootdockersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
