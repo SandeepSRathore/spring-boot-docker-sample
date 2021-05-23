597949f776373ca537a769edbb47fe70f46a29d5
SandeepSRathore <sandeeprathore9211@gmail.com>
2021-05-23 16:41:38 +0530

    Added greeting api

--- src/main/java/com/sr/springbootdockersample/SpringBootDockerSampleApplication.java
+++ src/main/java/com/sr/springbootdockersample/SpringBootDockerSampleApplication.java
@@ -2,12 +2,20 @@ package com.sr.springbootdockersample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerSampleApplication.class, args);
+	}
+
+	@GetMapping("/greetings")
+	public String greetings(){
+		return "Hello World!";
 	}
 
 }
